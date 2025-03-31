77b22d04cda8d36260d3a2160f9428cc3ffb9ca1
msadikm <mosadikhardoi@gmail.com>
2025-03-31 12:59:23 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -19,6 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI is running!"}
+
 
 @app.post("/api/")
 async def process_question(
